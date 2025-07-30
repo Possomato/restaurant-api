5fc006836e590d62522401a41ca1d663f568d32a
Possomato <gabrielb.possomato@gmail.com>
2025-07-30 09:34:23 -0300

    add - first base

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import Express from 'express'
+import { routes } from './routes'
+import { errorHandling } from './middlewares/error-handling'
 
 const app = Express()
 const PORT = 7070
 
-app.get('/', (req,res) => res.send('hello world'))
+app.use(Express.json())
+
+app.use(routes)
+
+app.use(errorHandling)
 
 app.listen(PORT, () => console.log(`i'm running on http://localhost:${PORT}`))
