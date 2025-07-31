6a655ad4aa1854e6cf75886403aad97036789d0b
Possomato <gabrielb.possomato@gmail.com>
2025-07-31 14:02:02 -0300

    add - remove products

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { knex } from '@/database/knex'
 import { z } from 'zod'
+import { AppError } from '@/utils/AppError'
 
 export class ProductsController {
   async create(req: Request, res: Response, next: NextFunction) {
@@ -48,6 +49,30 @@ export class ProductsController {
 
       await knex<ProductTable>('products').update({ name, price }).where({ id })
 
+      res.json()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  async remove(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: 'id must be a number' })
+        .parse(req.params.id)
+
+      const product = await knex<ProductTable>('products')
+        .select()
+        .where({ id })
+        .first()
+
+      if(!product){
+        throw new AppError('Product not found')
+      }
+
+      await knex<ProductTable>('products').delete().where({ id })
       res.json()
     } catch (e) {
       next(e)
