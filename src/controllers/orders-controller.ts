23790a238ab03262b062d238ee37f53ca82ff929
Possomato <gabrielb.possomato@gmail.com>
2025-08-02 11:49:18 -0300

    add - orders migration/controller/routes

---
+++ src/controllers/orders-controller.ts
@@ -0,0 +1,42 @@
+import { knex } from '@/database/knex'
+import { AppError } from '@/utils/AppError'
+import { Request, Response, NextFunction } from 'express'
+import { z } from 'zod'
+
+class OrdersController {
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        table_session_id: z.number(),
+        product_id: z.number(),
+        quantity: z.number(),
+      })
+
+      const { table_session_id, product_id, quantity } = bodySchema.parse(
+        req.body,
+      )
+
+      const session = await knex<TablesSessionsTable>('tables_sessions')
+        .where({ id: table_session_id })
+        .first()
+
+      if (!session || session.closed_at) {
+        throw new AppError('Not found an open table with this id')
+      }
+
+      const product = await knex<ProductTable>('products')
+        .where({ id: product_id })
+        .first()
+
+      if (!product) {
+        throw new AppError('product not found')
+      }
+
+      res.status(201).json({ session })
+    } catch (e) {
+      next(e)
+    }
+  }
+}
+
+export { OrdersController }
